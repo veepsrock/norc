a5120663169e0fddfb070f3c196dd1c597065355
veepsrock <vpeng@rockfound.org>
2023-11-29 12:10:54 -0500

    starting EDA file

--- eda.R
+++ eda.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(haven)
+library(janitor)
 
 
 # Read in data
-df <- read_sav("data/nov/[9800] Amplify AAPI M3 November 2023 - Final Data.sav")
+df <- read_sav("data/nov/[9800] Amplify AAPI M3 November 2023 - Final Data.sav") |> clean_names()
