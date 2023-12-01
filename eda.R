338ce4bea5fb463198f8f57422d8912f22811bbb
Torchi1 <torchi@rockfound.org>
2023-12-01 13:36:55 -0500

    Update eda.R

--- eda.R
+++ eda.R
@@ -4,4 +4,4 @@ library(janitor)
 
 
 # Read in data
-df <- read_sav("data/nov/[9800] Amplify AAPI M3 November 2023 - Final Data.sav") |> clean_names()
+df <- read_sav("/Users/torchi/Documents/GitHub/norc/data/nov/[9800] Amplify AAPI M3 November 2023 - Final Data.sav") |> clean_names()
