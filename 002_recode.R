e12cfbe53a2b3bcb76cca8ea3fd7ffc1685a686f
veepsrock <vpeng@rockfound.org>
2024-01-30 14:49:03 -0500

    updating recode function so it creates a new labeled column instead of writing over age4, coo, and asianorigin

--- 002_recode.R
+++ 002_recode.R
@@ -4,13 +4,13 @@ source("001_data_processing.R")
 
 # Recode values
 df <- df |> mutate(
-  age4= case_when(age4 ==1 ~ "18-29",
+  age4_l= case_when(age4 ==1 ~ "18-29",
                   age4 ==2 ~ "30-44",
                   age4 ==3 ~ "45-59",
                   age4 ==4 ~ "60+",
                   age4 == 9 ~ "Under 18"
   ),
-  asianorigin = case_when(asianorigin ==1 ~ "Chinese",
+  asianorigin_l = case_when(asianorigin ==1 ~ "Chinese",
                           asianorigin ==2 ~ "Asian Indian",
                           asianorigin ==3 ~ "Filipino",
                           asianorigin ==4 ~ "Vietnamese",
@@ -21,7 +21,7 @@ df <- df |> mutate(
                           asianorigin ==9 ~ "Multiple AAPI"
                           
   ),
-  coo = case_when(coo == 1 ~ "In the US",
+  coo_l = case_when(coo == 1 ~ "In the US",
                   coo == 2 ~ "Outside the US",
                   coo > 2 ~ "Don't know/Skipped/Refused"                 
   )
