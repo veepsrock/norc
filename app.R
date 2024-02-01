0637be134cd5ac7f65ce6b0ff62187ee5589e811
veepsrock <vpeng@rockfound.org>
2024-02-01 15:28:53 -0500

    adding shiny app for interactive visuals

---
+++ app.R
@@ -0,0 +1,117 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(haven)
+library(janitor)
+library(expss)
+library(survey)
+library(DT)
+
+
+# Read in data -------------------------------------------------------------------
+
+source("002_recode.R")
+
+# fuction for chisquare
+chisq_fx <- function(demo, ind_var){
+  
+  chisq <- chisq.test(table(df[[demo]], df[[ind_var]]))
+  sig <- if_else(chisq$p.value <0.05, "a significant", "not a significant")
+  print(chisq)
+  print(paste("There is", sig, "result between", demo, "and", ind_var))
+}
+
+# create survey object
+svy <- svydesign(ids=~1, weights = ~weight, data = df)
+
+
+
+# create list of demo 
+demo_list <- c("asianorigin_l", "income4_l", "region4_l")
+
+# create a list of independent variables
+ind_vars <- c("fs", "ns")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Relationship between demographic group and food security"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+          radioButtons("demo", "Select demographic of interest", choices = demo_list),
+          radioButtons("ind_var", "Select independent variable", choices = c("food security" = "fs", "nutrition security" = "ns")),
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          plotOutput("bar_plot"),
+          DTOutput("demo_dt")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  # Getting raw counts for observerations -----------------------------------------
+  
+  # write function for raw counts
+  table_fx <- function(demo, ind_var) {
+    df |>
+    group_by(.data[[ind_var]], .data[[demo]]) |> 
+    summarise(count = n())  |>
+    group_by(.data[[demo]])
+   # mutate(proportional_count = count / sum(count)) 
+  }
+  
+  # create demo table
+  demo_table <- reactive({table_fx(input$demo, input$ind_var)})
+  
+  # render data table output
+  output$demo_dt<- renderDT(demo_table(), options=list( info = FALSE, paging = F, searching = F))
+  
+  # Creating survey objects to use weights  -----------------------------------------
+  
+  # write function for generating survey objects
+  svy_fx <- function(demo, ind_var){
+    svy_object <- svyby(
+      formula = as.formula(paste0("~", ind_var)),
+      by = as.formula(paste0("~", demo)),
+      design = svy,
+      FUN = svymean
+    ) |> as_tibble()
+    
+  }
+  
+ # create survey object
+  svy_object <- reactive({
+    svy_fx(input$demo, input$ind_var)
+  })
+  
+  # Create bar plot for survey objects  -----------------------------------------
+  
+  # write function for generating plots
+  plot_fx <- function(demo, ind_var){
+    var_y <- ifelse(ind_var == "fs", "fslow", "nslow")
+    ggplot(svy_object(), aes(x=.data[[demo]], y = .data[[var_y]], fill =  '#00BFC4')) + 
+      geom_bar(stat="identity", show.legend=F) 
+  }
+  
+  output$bar_plot = renderPlot({
+    plot_fx(input$demo, input$ind_var)
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
